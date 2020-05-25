1c54cc03c5946aad0e99fcfdb3a84d5ad9ee0640
EC2 Default User <ec2-user@ip-172-31-44-171.ap-northeast-1.compute.internal>
2020-05-25 13:38:13 +0000

    Add user following

--- config/application.rb
+++ config/application.rb
@@ -1,3 +1,4 @@
+require File.expand_path('../boot', __FILE__)
 require_relative 'boot'
 
 require 'rails/all'
@@ -10,9 +11,13 @@ module HelloApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+  end
+end
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+module SampleApp
+  class Application < Rails::Application
+    
+    # 認証トークンをremoteフォームに埋め込む
+    config.action_view.embed_authenticity_token_in_remote_forms = true
   end
 end
